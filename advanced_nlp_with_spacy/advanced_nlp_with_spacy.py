9ea562d68264ca0d9fca9d92de0c8a77137e0fd6
gggrv <43758914+gggrv@users.noreply.github.com>
2021-01-20 00:05:43 +0200

    Add files via upload

--- advanced_nlp_with_spacy/advanced_nlp_with_spacy.py
+++ advanced_nlp_with_spacy/advanced_nlp_with_spacy.py
@@ -118,9 +118,153 @@ def test3_en():
         print( doc[L:R] )
         
 """-------------------------------------------------------------------------+++
+Everything about chapter2.
+"""
+def ch2test1_ru():
+    #stanza.download('ru') # already done # download rus statistical model
+    
+    snlp = stanza.Pipeline( lang='ru' )
+    nlp = spacy_stanza.StanzaLanguage( snlp )
+    
+    wordstr = 'программа'
+    wordhash = nlp.vocab.strings[wordstr]
+    print( 'Hash test RU for word "%s":'%wordstr, wordhash )
+    try:
+        reverse = nlp.vocab.strings[wordhash]
+        print( 'Looking up string via hash↑: %s ...it works.'%reverse )
+    except Exception as ex:
+        print( "Looking up string via hash↑... it doesn't work.")
+        print( ex )
+
+def ch2test1_en():
+    nlp = spacy.load('en_core_web_md')
+    
+    wordstr = 'program'
+    wordhash = nlp.vocab.strings[wordstr]
+    print( 'Hash test EN for word "%s":'%wordstr, wordhash )
+    try:
+        reverse = nlp.vocab.strings[wordhash]
+        print( 'Looking up string via hash↑: %s ...it works.'%reverse )
+    except Exception as ex:
+        print( "Looking up string via hash↑... it doesn't work.")
+        print( ex )
+        
+def ch2test2_ru():
+    #stanza.download('ru') # already done # download rus statistical model
+    
+    snlp = stanza.Pipeline( lang='ru' )
+    nlp = spacy_stanza.StanzaLanguage( snlp )
+    
+    words = [ 'Плевать', 'какой', 'текст' ]
+    spaces = [ True, True, False ]
+    doc = spacy.tokens.Doc( nlp.vocab, words, spaces )
+    print( doc.text )
+    
+    span = spacy.tokens.Span( doc, 1,3, label='intentionally_wrong' )
+    doc.ents = [span]
+    
+    markup = displacy.render( doc, style='ent', page=True, jupyter=False )
+    savef( 'ent_ru.html', markup )
+
+def ch2test2_en():
+    nlp = spacy.load('en_core_web_md')
+    
+    words = [ 'Whatever', 'this', 'is' ]
+    spaces = [ True, True, False ]
+    doc = spacy.tokens.Doc( nlp.vocab, words, spaces )
+    print( doc.text )
+    
+    span = spacy.tokens.Span( doc, 0,2, label='intentionally_wrong' )
+    doc.ents = [span]
+    
+    markup = displacy.render( doc, style='ent', page=True, jupyter=False )
+    savef( 'ent_en.html', markup )
+        
+def ch2test3_ru():
+    #stanza.download('ru') # already done # download rus statistical model
+    
+    snlp = stanza.Pipeline( lang='ru' )
+    nlp = spacy_stanza.StanzaLanguage( snlp )
+    
+    words = [ 'Плевать', 'какой', 'текст' ]
+    spaces = [ True, True, False ]
+    doc = spacy.tokens.Doc( nlp.vocab, words, spaces )
+    
+    print( doc.text )
+    print( len(doc.vector) )
+
+def ch2test3_en():
+    nlp = spacy.load('en_core_web_md')
+    
+    words = [ 'Whatever', 'this', 'is' ]
+    spaces = [ True, True, False ]
+    doc = spacy.tokens.Doc( nlp.vocab, words, spaces )
+    
+    print( doc.text )
+    print( len(doc.vector) )
+        
+def ch2test4_ru():
+    print( "For this RU model it doesn't work yet." )
+    return None
+    #stanza.download('ru') # already done # download rus statistical model
+    
+    snlp = stanza.Pipeline( lang='ru' )
+    nlp = spacy_stanza.StanzaLanguage( snlp )
+    
+    words = [ 'Плевать', 'какой', 'текст' ]
+    spaces = [ True, True, False ]
+    doc1 = spacy.tokens.Doc( nlp.vocab, words, spaces )
+    words = [ 'Чай', 'и', 'кофе' ]
+    spaces = [ True, True, False ]
+    doc2 = spacy.tokens.Doc( nlp.vocab, words, spaces )
+    
+    print( doc1.text )
+    print( doc2.text )
+    print( doc1.similarity(doc2) )
+
+def ch2test4_en():
+    nlp = spacy.load('en_core_web_md')
+    
+    words = [ 'Whatever', 'this', 'is' ]
+    spaces = [ True, True, False ]
+    doc1 = spacy.tokens.Doc( nlp.vocab, words, spaces )
+    words = [ 'Tea', 'and', 'coffee' ]
+    spaces = [ True, True, False ]
+    doc2 = spacy.tokens.Doc( nlp.vocab, words, spaces )
+    
+    print( doc1.text )
+    print( doc2.text )
+    print( doc1.similarity(doc2) )
+        
+"""-------------------------------------------------------------------------+++
+Everything about chapter3.
+"""
+def ch3test1_ru():
+    #stanza.download('ru') # already done # download rus statistical model
+    
+    snlp = stanza.Pipeline( lang='ru' )
+    nlp = spacy_stanza.StanzaLanguage( snlp )
+    
+    #doc = spacy.tokens.Doc( readf(TXT_RU) )
+    
+    print( nlp.pipe_names )
+    print( nlp.pipeline )
+
+def ch3test1_en():
+    nlp = spacy.load('en_core_web_md')
+    
+    #doc = spacy.tokens.Doc( readf(TXT_EN) )
+    
+    print( nlp.pipe_names )
+    print( nlp.pipeline )
+        
+"""-------------------------------------------------------------------------+++
 autorun
 """
 def autorun():
+    """---------------------------------------------------------------------+++
+    Chapter 1.
+    """
     """
     # autorun test 1
     # creates two docs and seeks the value before the '%'
@@ -142,7 +286,7 @@ def autorun():
     savef( 'dep.html', markup )
     #"""
     
-    #"""
+    """
     # autorun test 3
     # creates two docs
     # applies to them corresponding statistical models
@@ -150,9 +294,49 @@ def autorun():
     test3_ru()
     test3_en()
     #"""
+    
+    """---------------------------------------------------------------------+++
+    Chapter 2.
+    """
+    """
+    # autorun test 1
+    # test nlp.vocab.strings lookup table
+    ch2test1_ru()
+    ch2test1_en()
+    #"""
+    
+    """
+    # autorun test 2
+    # manually construct doc and span object
+    ch2test2_ru()
+    ch2test2_en()
+    #"""
+    
+    """
+    # autorun test 3
+    # see if 300D word vectors work
+    ch2test3_ru()
+    ch2test3_en()
+    #"""
+    
+    """
+    # autorun test 4
+    # compare two documents
+    ch2test4_ru()
+    ch2test4_en()
+    #"""
+    """---------------------------------------------------------------------+++
+    Chapter 3.
+    """
+    #"""
+    # autorun test 1
+    # compare two documents
+    ch3test1_ru()
+    ch3test1_en()
+    #"""
 
 if __name__ == '__main__':
     autorun()
     
 #---------------------------------------------------------------------------+++
-# конец 2021.01.13 → 2021.01.13
+# конец 2021.01.13 → 2021.01.19
